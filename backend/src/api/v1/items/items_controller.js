e9c108482d0d99220b54fedb6414ba9178ea6ed1
fepu08 <ferencbesenyei@protonmail.com>
2025-02-15 20:18:05 +0100

    added get item by ID to items API

--- backend/src/api/v1/items/items_controller.js
+++ backend/src/api/v1/items/items_controller.js
@@ -1,9 +1,23 @@
 const getRandomItems = require('../../../mock/items.js');
+const items = getRandomItems();
 
 function getAllItems(req, res) {
   console.log('Getting all items...');
-  const items = getRandomItems();
-  res.json(...items);
+  res.json([...items]);
 }
 
-module.exports = { getAllItems };
+function getItemById(req, res) {
+  console.log(`Getting item by ID: ${req.params.id}`);
+  if (!req.params.id) {
+    res.status(400).json({ msg: 'No ID provided' });
+    return;
+  }
+  const item = items.find((item) => item.id === req.params.id);
+  if (!item) {
+    res.status(404).json({ msg: 'No items found' });
+    return;
+  }
+  res.json(item);
+}
+
+module.exports = { getAllItems, getItemById };
--- backend/src/api/v1/items/items_router.js
+++ backend/src/api/v1/items/items_router.js
@@ -3,5 +3,6 @@ const itemsController = require('./items_controller.js');
 const router = express.Router();
 
 router.get('/', itemsController.getAllItems);
+router.get('/:id', itemsController.getItemById);
 
 module.exports = router;
