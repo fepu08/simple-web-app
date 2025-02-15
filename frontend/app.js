94d899d858d02eb4e04cadb352dac7f4b3d533e7
fepu08 <ferencbesenyei@protonmail.com>
2025-02-15 21:15:34 +0100

    list the items from the API

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,51 @@
-console.log('Hello World');
+class ApiClient {
+  constructor(baseUrl) {
+    this.baseUrl = baseUrl;
+  }
+
+  async getItems() {
+    const res = await fetch(this.baseUrl + '/items');
+    if (!res.ok) {
+      console.error('Something went wrong...');
+    }
+    return res.json();
+  }
+}
+
+function createTextFromItemObj(item) {
+  const parts = [];
+  Object.entries(item).forEach(([key, value]) => {
+    parts.push(`${key}: ${value}`);
+  });
+
+  return parts.join(', ');
+}
+
+(async function () {
+  const apiBaseUrl = 'http://localhost:3000';
+  const itemsContainer = document.getElementById('items-container');
+  const apiClient = new ApiClient(apiBaseUrl);
+  const items = await apiClient.getItems();
+
+  if (!items || items.length < 1) {
+    const error = document.createElement('h2');
+    error.innerText = 'No items to show';
+    itemsContainer.appendChild(error);
+  }
+
+  const itemsList = document.createElement('ul');
+  for (let i = 0; i < items.length; i++) {
+    const outerListItem = document.createElement('li');
+    const innerList = document.createElement('ul');
+    outerListItem.appendChild(innerList);
+
+    Object.entries(items[i]).forEach(([key, value]) => {
+      const innerListItem = document.createElement('li');
+      innerListItem.innerText = `${key} - ${value}`;
+      innerList.appendChild(innerListItem);
+    });
+
+    itemsList.appendChild(outerListItem);
+  }
+  itemsContainer.appendChild(itemsList);
+})();
